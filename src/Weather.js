fac546a1750df33bdca6a182711a26617bd17a30
CamilaDelagnese <camiladelagnese22@gmail.com>
2021-03-31 19:15:09 +0100

    add some changes

---
+++ src/Weather.js
@@ -0,0 +1,22 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props){
+    function handleResponse(response) {
+        alert('The weather in ${response.data.name} is ${response.data.main.temp}°C');
+    }
+    let apiKey = "462b3280dd5162d38a5d70ad632ffddd";
+    let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric';
+
+    axios.get(apiUrl).then(handleResponse);
+    return <h2> Hello again !</h2>
+
+}
+<small>
+<a
+  href="https://github.com/CamilaDelagnese/weather-app-react"
+  target="_blank"
+  >Open-source code</a
+>
+by Camila Delagnese
+</small>
