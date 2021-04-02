6ca88f534128679856c0f68cab737e178dbb2323
CamilaDelagnese <camiladelagnese22@gmail.com>
2021-04-02 13:49:28 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Weather from "./Weather";
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> hello</h1>
-        <Weather city="London"/>
+        <Weather city="London" />
       </header>
+      <small>
+        <a
+          href="https://github.com/CamilaDelagnese/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        by Camila Delagnese
+      </small>
     </div>
   );
 }
-
 export default App;
